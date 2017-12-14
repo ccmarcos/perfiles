7833313024fd0ede6591d522c7c3f2accb48788c
aquinovo <vela.aquino@gmail.com>
2017-12-14 15:38:41 -0600

    defined layout

--- src/index.js
+++ src/index.js
@@ -79,8 +79,53 @@ class ModificandoDiv extends React.Component {
                     <div className="video-bloque1">
                       <div className="info-bloque1"></div>
                     </div>
+                    <div className="video-bloque2">
+                      <div className="circleBase type1">
+                        <div className="triangle-right"></div>
+                      </div>
+                    </div>
+                    <div className="video-bloque3">
+                      <div className="video-bloque31">
+                        <div className="bloque-item1"></div>
+                        <div className="bloque-item2"></div>
+                        <div className="bloque-item3"></div>
+                      </div>
+                      <div className="video-bloque32">
+                        <div className="video-item1"></div>
+                        <div className="video-item2"></div>
+                      </div>
+                      <div className="video-bloque33">
+                        <div className="video-item11"></div>
+                        <div className="video-item22"></div>
+                      </div>
+                    </div>
+                    <div className="bloque4">
+                      <div className="bloque-morado"></div>
+                      <div className="bloque-morado2"></div>
+                    </div>
                   </div>
-                  <div className="seccion-izquierdo"></div>
+                  <div className="seccion-izquierdo">
+                    <div className="post-bloque1">
+                      <div className="post-bloque11"></div>
+                    </div>
+                    <div className="post-bloque2"></div>
+                    <div className="post-bloque3"></div>
+                    <div className="post-publicado">
+                      <div className="publicado-perfil">
+                        <div className="publicado-perfil-info1"></div>
+                        <div className="publicado-perfil-info2">
+                          <div className="publicado-item1"></div>
+                          <div className="publicado-item2"></div>
+                        </div>
+                        <div className="publicado-guardado"></div>
+                      </div>
+                      <div className="descripcion-publicacion1"></div>
+                      <div className="descripcion-publicacion2"></div>
+                      <div className="descripcion-publicacion3"></div>
+                      <div className="descripcion-publicacion4"></div>
+                      <div className="publicacion-foto"></div>
+                    </div>
+                  </div>
                 </div>
             </div>
             <div className="contenedor-izquierdo">
